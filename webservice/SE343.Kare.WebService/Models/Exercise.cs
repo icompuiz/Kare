40009412ce319830d65cd7566828913c55883d48
Alex <acc1728@rit.edu>
2014-04-21 19:04:34 -0400

    Initial commit for exercise and related models

---
+++ webservice/SE343.Kare.WebService/Models/Exercise.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SE343.Kare.WebService.Models
+{
+    public class Exercise
+    {
+        private List<Instruction> _instructions;
+        private Quiz _quiz;
+
+        public List<Instruction> Instructions
+        {
+            get { return _instructions; }
+            set { _instructions = value; }
+        }
+
+        public Quiz Quiz
+        {
+            get { return _quiz; }
+            set { _quiz = value; }
+        }
+    }
+}
---
+++ webservice/SE343.Kare.WebService/Models/Quiz.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SE343.Kare.WebService.Models
+{
+    public class Quiz
+    {
+    }
+}
