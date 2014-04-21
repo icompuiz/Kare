35bcf904f2cefc57e111479a584e7a4295fda2ea
Alex <acc1728@rit.edu>
2014-04-21 19:42:07 -0400

    Added a name property to the Exercise

--- webservice/SE343.Kare.WebService/Models/Exercise.cs
+++ webservice/SE343.Kare.WebService/Models/Exercise.cs
@@ -7,19 +7,25 @@ namespace SE343.Kare.WebService.Models
 {
     public class Exercise
     {
-        private List<Instruction> _instructions;
-        private Quiz _quiz;
-
+        private List<Instruction> _instructions
         public List<Instruction> Instructions
         {
             get { return _instructions; }
             set { _instructions = value; }
         }
 
+        private Quiz _quiz;
         public Quiz Quiz
         {
             get { return _quiz; }
             set { _quiz = value; }
+        }
+
+        private String _name;
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
         }
     }
 }
